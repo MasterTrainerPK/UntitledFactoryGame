dd6b7246b0dba905f1fc75a75ad0e39c97d69f27
MasterTrainerPK <perrylovesbooks@gmail.com>
2023-08-03 23:05:27 +0000

    Pivot to Vulkan
    SDL2 documentation is weird
    Vulkan doumentation is verbose

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # An awesome codebase that may one day be a game
 
 ## Roadmap
- - use sdl
+ - use Vulkan
  - use c
  - figure out what we are doing
--- main.c
+++ main.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
+#include <vulkan/vulkan.h>
+#include <GLFW/glfw3.h>
+
+void error_handle_glfw(int e, const char* msg) {
+    fprintf(stderr, "GLFW ERR: %d, MSG: %s", e, msg);
+}
 
 int main() {
-	printf("Hello World!");
-
-	return 0;
+    glfwInit();
+    glfwSetErrorCallback(&error_handle_glfw);
+    GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+    if (!monitor) {
+        perror("The program screams in panic as it cannot find your primary monitor");
+        return -1;
+    }
+    GLFWwindow* window = NULL;
+    glfwCreateWindow(1000, 1000, "Hello Window", NULL, window);
+    if (!window) {
+       perror("ERR: no window...");
+    }
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    printf("Program Exiting Normally\n");
+    return 0;
 }
