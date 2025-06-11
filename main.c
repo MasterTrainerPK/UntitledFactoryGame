d13ecea2a546817037a6bf22259dc3aa4b5e42bb
Discordian <62210084+Discordiann@users.noreply.github.com>
2025-06-11 14:24:09 -0500

    Fix more things

--- main.c
+++ main.c
@@ -173,7 +173,7 @@ int main() {
         error_code = -1;
         goto destory_device;
     }
-    GLFWwindow* window = glfwCreateWindow(1000, 1000, "Hello Window", monitor, NULL);
+    GLFWwindow* window = glfwCreateWindow(1000, 1000, "Hello Window", NULL, NULL);
     if (window == NULL) {
         perror("ERR: no window...");
         error_code = -1;
@@ -201,12 +201,12 @@ int main() {
                         .flags = 0x0,
                         .surface = surface,
                         .minImageCount = 1,
-                        .imageFormat = VK_FORMAT_UNDEFINED,
+                        .imageFormat = VK_FORMAT_B8G8R8A8_SRGB,
                         .imageColorSpace = VK_COLOR_SPACE_SRGB_NONLINEAR_KHR,
                         .imageExtent = image_extent,
                         .imageArrayLayers = 1,
                         .imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT,
-                        .imageSharingMode = VK_SHARING_MODE_CONCURRENT,
+                        .imageSharingMode = VK_SHARING_MODE_EXCLUSIVE,
                         .queueFamilyIndexCount = queue_family_index,
                         .pQueueFamilyIndices = queue_family_indices,
                         .preTransform = 0x0,
@@ -245,7 +245,7 @@ int main() {
     );
     VkAttachmentDescription attachment_description = (VkAttachmentDescription) {
                         .flags = 0x0,
-                        .format = VK_IMAGE_ASPECT_COLOR_BIT,
+                        .format = VK_FORMAT_B8G8R8A8_SRGB,
                         .samples = VK_SAMPLE_COUNT_1_BIT,
                         .loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR,
                         .storeOp = VK_ATTACHMENT_STORE_OP_STORE,
@@ -313,14 +313,14 @@ int main() {
                         .pNext = NULL,
                         .flags = 0x0,
                         .imageType = VK_IMAGE_TYPE_2D,
-                        .format = VK_FORMAT_UNDEFINED,
+                        .format = VK_FORMAT_B8G8R8A8_SRGB,
                         .extent = extent,
                         .mipLevels = 1,
                         .arrayLayers = 1,
-                        .samples = 0x0,
+                        .samples = VK_SAMPLE_COUNT_1_BIT,
                         .tiling = VK_IMAGE_TILING_OPTIMAL,
-                        .usage = 0x0,
-                        .sharingMode = VK_SHARING_MODE_CONCURRENT,
+                        .usage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT,
+                        .sharingMode = VK_SHARING_MODE_EXCLUSIVE,
                         .queueFamilyIndexCount = 1,
                         .pQueueFamilyIndices = queue_family_indices,
                         .initialLayout = VK_IMAGE_LAYOUT_UNDEFINED
@@ -328,15 +328,27 @@ int main() {
                     NULL,
                     &image
     );
+    int image_count;
+    vkGetSwapchainImagesKHR( device,
+                    swapchain,
+                    &image_count,
+                    NULL
+    );
+    VkImage *swapchain_image_array = malloc(image_count);
+    vkGetSwapchainImagesKHR( device,
+                    swapchain,
+                    &image_count,
+                    swapchain_image_array
+    );
     VkImageView image_view;
     vkCreateImageView( device,
                     &(VkImageViewCreateInfo) {
                         .sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO,
                         .pNext = NULL,
                         .flags = 0x0,
-                        .image = image,
+                        .image = swapchain_image_array[0],
                         .viewType = VK_IMAGE_VIEW_TYPE_2D,
-                        .format = VK_FORMAT_UNDEFINED,
+                        .format = VK_FORMAT_B8G8R8A8_SRGB,
                         .components = (VkComponentMapping) {
                             .r = VK_COMPONENT_SWIZZLE_R,
                             .g = VK_COMPONENT_SWIZZLE_G,
@@ -344,10 +356,10 @@ int main() {
                             .a = VK_COMPONENT_SWIZZLE_A,
                         },
                         .subresourceRange = (VkImageSubresourceRange) {
-                            .aspectMask = 0x0,
-                            .baseMipLevel = 1,
+                            .aspectMask = VK_IMAGE_ASPECT_NONE_KHR,
+                            .baseMipLevel = 0,
                             .levelCount = 1,
-                            .baseArrayLayer = 1,
+                            .baseArrayLayer = 0,
                             .layerCount = 1
                         }
                     },
@@ -394,7 +406,7 @@ int main() {
     long fsize = ftell(f);
     fseek(f, 0, SEEK_SET);
     char *vert_shader_code = malloc(fsize);
-    fread(vert_shader_code, CHAR_BIT, fsize, f);
+    fread(vert_shader_code, sizeof(char), fsize, f);
     fclose(f);
     printf(vert_shader_code);
 
