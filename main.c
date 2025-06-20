be9b3811b9ddcf95e3f458868fdda70bfd1472ed
Discordian <62210084+Discordiann@users.noreply.github.com>
2025-06-20 11:55:53 -0500

    Move code into render loop

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <math.h>
 #include <vulkan/vulkan.h>
 #include <GLFW/glfw3.h>
 #include <vulkan/vulkan_core.h>
@@ -387,6 +388,7 @@ int main() {
     FILE *f_model = fopen("cube.obj", "r");
 
     int vertex_count = 3;
+    int vertex_size = 4;
     float vertex_1[4] = {0.0f, -0.5f, 0.0f, 1.0f};
     float vertex_2[4] = {0.5f, 0.5f, 0.0f, 1.0f};
     float vertex_3[4] = {-0.5f, 0.5f, 0.0f, 1.0f};
@@ -394,11 +396,11 @@ int main() {
     float vertices[12] = {
         0.0f, -0.5f, 0.0f, 1.0f,
         0.5f, 0.5f, 0.0f, 1.0f,
-        0.0f, 0.5f, 0.0f, 1.0f };
+        -0.5f, 0.5f, 0.0f, 1.0f };
 
     VkVertexInputBindingDescription vertex_binding_description = (VkVertexInputBindingDescription) {
         .binding = 0,
-        .stride = sizeof(float)*4,
+        .stride = sizeof(float) * vertex_size,
         .inputRate = VK_VERTEX_INPUT_RATE_VERTEX
     };
 
@@ -416,7 +418,7 @@ int main() {
             .sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO,
             .pNext = NULL,
             .flags = 0x0,
-            .size = sizeof(float)* 4 * vertex_count,
+            .size = sizeof(float) * vertex_size * vertex_count,
             .usage = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
             .sharingMode = VK_SHARING_MODE_EXCLUSIVE,
             .queueFamilyIndexCount = 1,
@@ -456,8 +458,7 @@ int main() {
     vkBindBufferMemory(device, vertex_buffer, vertex_memory, 0);
 
     void *data;
-    vkMapMemory(device, vertex_memory, 0, sizeof(float) * 4 * vertex_count, 0x0, &data);
-    memcpy(data, vertices, memory_requirements.size);
+    vkMapMemory(device, vertex_memory, 0, sizeof(float) * vertex_size * vertex_count, 0x0, &data);
 
     FILE *f_vertex = fopen("shaders/vert.spv", "rb");
     if(f_vertex == NULL) {
@@ -665,142 +666,74 @@ int main() {
     ), destroy_pipeline_layout);
 
     printf("%s", "Pipeline created\n");
-    handle_error(vkBeginCommandBuffer(
-        command_buffer,
-        &(VkCommandBufferBeginInfo) {
-            .sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO,
-            .pNext = NULL,
-            .flags = 0x0,
-            .pInheritanceInfo = &(VkCommandBufferInheritanceInfo) {
-                .sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_INHERITANCE_INFO,
-                .pNext = NULL,
-                .renderPass = render_pass,
-                .subpass = 0,
-                .framebuffer = framebuffer,
-                .occlusionQueryEnable = VK_TRUE,
-                .queryFlags = 0x0,
-                .pipelineStatistics = 0x0,
-            }
-        }
-    ), destroy_pipeline);
 
-    VkClearValue clear_color = {{0.0f, 0.0f, 0.0f, 1.0f}};
-    VkClearValue *clear_color_array = malloc(sizeof(VkClearValue) * image_count);
-    for(int i = 0; i < image_count; i++) {
-        clear_color_array[i] = clear_color;
+    VkSemaphore *image_available_semaphore_array = malloc(sizeof(VkSemaphore) * image_count);
+    int created_image_semaphores;
+    for (created_image_semaphores = 0; created_image_semaphores < image_count; created_image_semaphores++) {
+        VkSemaphore image_available_semaphore;
+        handle_error(vkCreateSemaphore(
+            device,
+            &(VkSemaphoreCreateInfo) {
+                .sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO,
+                .pNext = NULL,
+                .flags = 0x0
+            },
+            NULL,
+            &image_available_semaphore
+        ), destroy_pipeline);
+        image_available_semaphore_array[created_image_semaphores] = image_available_semaphore;
     }
 
-    vkCmdBeginRenderPass(
-        command_buffer,
-        &(VkRenderPassBeginInfo) {
-            .sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO,
-            .pNext = NULL,
-            .renderPass = render_pass,
-            .framebuffer = framebuffer,
-            .renderArea = (VkRect2D) {
-                .offset = {0, 0},
-                .extent = image_extent
+    VkSemaphore *render_finished_semaphore_array = malloc(sizeof(VkSemaphore) * image_count);
+    int created_render_semaphores;
+    for (created_render_semaphores = 0; created_render_semaphores < image_count; created_render_semaphores++) {
+        VkSemaphore render_finished_semaphore;
+        handle_error(vkCreateSemaphore(
+            device,
+            &(VkSemaphoreCreateInfo) {
+                .sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO,
+                .pNext = NULL,
+                .flags = 0x0
             },
-            .clearValueCount = image_count,
-            .pClearValues = clear_color_array
-        },
-        VK_SUBPASS_CONTENTS_INLINE
-    );
-    printf("%s", "Command buffer and render pass have begun\n");
+            NULL,
+            &render_finished_semaphore
+        ), destroy_image_semaphores);
+        render_finished_semaphore_array[created_render_semaphores] = render_finished_semaphore;
+    }
 
-    VkSemaphore image_available_semaphore;
-    handle_error(vkCreateSemaphore(
-        device,
-        &(VkSemaphoreCreateInfo) {
-            .sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO,
-            .pNext = NULL,
-            .flags = 0x0
-        },
-        NULL,
-        &image_available_semaphore
-    ), destroy_pipeline);
+    VkFence *in_flight_fence_array = malloc(sizeof(VkFence) * image_count);
+    int created_in_flight_fences;
+    for (created_in_flight_fences = 0; created_in_flight_fences < image_count; created_in_flight_fences++) {
+        VkFence in_flight_fence;
+        handle_error(vkCreateFence(
+            device,
+            &(VkFenceCreateInfo) {
+                .sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO,
+                .pNext = NULL,
+                .flags = VK_FENCE_CREATE_SIGNALED_BIT
+            },
+            NULL,
+            &in_flight_fence
+        ), destroy_render_semaphores);
+        in_flight_fence_array[created_in_flight_fences] = in_flight_fence;
+    }
+    printf("%s", "Sync objects created\n");
 
-    VkSemaphore render_finished_semaphore;
-    handle_error(vkCreateSemaphore(
-        device,
-        &(VkSemaphoreCreateInfo) {
-            .sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO,
-            .pNext = NULL,
-            .flags = 0x0
-        },
-        NULL,
-        &render_finished_semaphore
-    ), destroy_image_semaphore);
+    uint32_t current_frame = 0;
 
-    VkFence in_flight_fence;
-    handle_error(vkCreateFence(
-        device,
-        &(VkFenceCreateInfo) {
-            .sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO,
-            .pNext = NULL,
-            .flags = 0x0
-        },
-        NULL,
-        &in_flight_fence
-    ), destroy_render_semaphore);
-
-    uint32_t image_index;
-    vkAcquireNextImageKHR(device, swapchain, UINT64_MAX, image_available_semaphore, VK_NULL_HANDLE, &image_index);
-    printf("%s", "Ready to submit commands\n");
-
-    vkCmdBindPipeline(command_buffer, VK_PIPELINE_BIND_POINT_GRAPHICS, pipeline);
-    VkDeviceSize offsets[1] = {0};
-    vkCmdBindVertexBuffers(command_buffer, 0, 1, &vertex_buffer, offsets);
-    vkCmdDraw(command_buffer, vertex_count, 1, 0, 0);
-    vkCmdEndRenderPass(command_buffer);
-    vkEndCommandBuffer(command_buffer);
-
-    VkPipelineStageFlags wait_stage_mask_array[1] = {VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT};
-    vkQueueSubmit(
-        queue,
-        1,
-        &(VkSubmitInfo) {
-            .sType = VK_STRUCTURE_TYPE_SUBMIT_INFO,
-            .pNext = NULL,
-            .waitSemaphoreCount = 1,
-            .pWaitSemaphores = &image_available_semaphore,
-            .pWaitDstStageMask = wait_stage_mask_array,
-            .commandBufferCount = 1,
-            .pCommandBuffers = &command_buffer,
-            .signalSemaphoreCount = 1,
-            .pSignalSemaphores = &render_finished_semaphore
-        },
-        in_flight_fence
-    );
-    printf("%s", "Commands submitted\n");
-    vkWaitForFences(device, 1, &in_flight_fence, VK_TRUE, UINT64_MAX);
-
-    vkQueuePresentKHR(
-        queue,
-        &(VkPresentInfoKHR) {
-            .sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR,
-            .pNext = NULL,
-            .waitSemaphoreCount = 1,
-            .pWaitSemaphores = &render_finished_semaphore,
-            .swapchainCount = 1,
-            .pSwapchains = &swapchain,
-            .pImageIndices = &image_index,
-            .pResults = NULL
-        }
-    );
     struct timespec curr_time;
     const int dt = 10000000; // 1/100 of a sec
     long int accumulator = 0;
     long int t = 0;
     if(clock_gettime(CLOCK_MONOTONIC, &curr_time) != 0) {
             perror("failed to get time");
-            goto destroy_flight_fence;
+            goto destroy_flight_fences;
     }
     while(!glfwWindowShouldClose(window) && glfwGetMouseButton(window, 1) != GLFW_PRESS) {
         struct timespec new_time;
         if(clock_gettime(CLOCK_MONOTONIC, &curr_time) != 0) {
             perror("failed to get time during loop");
-            goto destroy_flight_fence;
+            goto destroy_flight_fences;
         }
         int frame_time = new_time.tv_sec - curr_time.tv_sec;
         frame_time = new_time.tv_nsec - curr_time.tv_nsec + frame_time * 1000000000 ; // sec to ns
@@ -817,15 +750,125 @@ int main() {
         }
         const double alpha = (double)accumulator / dt;
         //lerp state and render state;
+        printf("%s", "Beginning new frame\n");
+
+        vkWaitForFences(device, 1, &in_flight_fence_array[current_frame], VK_TRUE, UINT64_MAX);
+        vkResetFences(device, 1, &in_flight_fence_array[current_frame]);
+        vkResetCommandBuffer(command_buffer, 0x0);
+
+        float *new_vertices = malloc(sizeof(float) * vertex_count);
+        for (int i = 0; i < vertex_count; i++) {
+            new_vertices[i * vertex_size] = cos(t);
+            new_vertices[i * vertex_size + 1] = sin(t);
+        }
+        memcpy(data, new_vertices, memory_requirements.size);
+
+        handle_error(vkBeginCommandBuffer(
+            command_buffer,
+            &(VkCommandBufferBeginInfo) {
+                .sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO,
+                .pNext = NULL,
+                .flags = 0x0,
+                .pInheritanceInfo = &(VkCommandBufferInheritanceInfo) {
+                    .sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_INHERITANCE_INFO,
+                    .pNext = NULL,
+                    .renderPass = render_pass,
+                    .subpass = 0,
+                    .framebuffer = framebuffer,
+                    .occlusionQueryEnable = VK_TRUE,
+                    .queryFlags = 0x0,
+                    .pipelineStatistics = 0x0,
+                }
+            }
+        ), destroy_flight_fences);
+
+        VkClearValue clear_color = {{0.0f, 0.0f, 0.0f, 1.0f}};
+        VkClearValue *clear_color_array = malloc(sizeof(VkClearValue) * image_count);
+        for(int i = 0; i < image_count; i++) {
+            clear_color_array[i] = clear_color;
+        }
+
+        vkCmdBeginRenderPass(
+            command_buffer,
+            &(VkRenderPassBeginInfo) {
+                .sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO,
+                .pNext = NULL,
+                .renderPass = render_pass,
+                .framebuffer = framebuffer,
+                .renderArea = (VkRect2D) {
+                    .offset = {0, 0},
+                    .extent = image_extent
+                },
+                .clearValueCount = image_count,
+                .pClearValues = clear_color_array
+            },
+            VK_SUBPASS_CONTENTS_INLINE
+        );
+        printf("%s", "Command buffer and render pass have begun\n");
+
+        uint32_t image_index;
+        vkAcquireNextImageKHR(device, swapchain, UINT64_MAX, image_available_semaphore_array[current_frame], VK_NULL_HANDLE, &image_index);
+        printf("%s", "Ready to submit commands\n");
+
+        vkCmdBindPipeline(command_buffer, VK_PIPELINE_BIND_POINT_GRAPHICS, pipeline);
+        VkDeviceSize offsets[1] = {0};
+        vkCmdBindVertexBuffers(command_buffer, 0, 1, &vertex_buffer, offsets);
+        vkCmdDraw(command_buffer, vertex_count, 1, 0, 0);
+        vkCmdEndRenderPass(command_buffer);
+        vkEndCommandBuffer(command_buffer);
+
+        VkPipelineStageFlags wait_stage_mask_array[1] = {VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT};
+        vkQueueSubmit(
+           queue,
+            1,
+                &(VkSubmitInfo) {
+                .sType = VK_STRUCTURE_TYPE_SUBMIT_INFO,
+                .pNext = NULL,
+                .waitSemaphoreCount = 1,
+                .pWaitSemaphores = &image_available_semaphore_array[current_frame],
+                .pWaitDstStageMask = wait_stage_mask_array,
+                .commandBufferCount = 1,
+                .pCommandBuffers = &command_buffer,
+                .signalSemaphoreCount = 1,
+                .pSignalSemaphores = &render_finished_semaphore_array[current_frame]
+            },
+            in_flight_fence_array[current_frame]
+        );
+        printf("%s", "Commands submitted\n");
+        vkWaitForFences(device, 1, &in_flight_fence_array[current_frame], VK_TRUE, UINT64_MAX);
+
+        vkQueuePresentKHR(
+            queue,
+            &(VkPresentInfoKHR) {
+                .sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR,
+                .pNext = NULL,
+                .waitSemaphoreCount = 1,
+                .pWaitSemaphores = &render_finished_semaphore_array[current_frame],
+                .swapchainCount = 1,
+                .pSwapchains = &swapchain,
+                .pImageIndices = &image_index,
+                .pResults = NULL
+            }
+        );
+        printf("%s", "Image presented\n");
+
+        current_frame = (current_frame + 1) % image_count;
+
         glfwPollEvents();
     }
     printf("Exiting normally!!\n\n");
-destroy_flight_fence:
-    vkDestroyFence(device, in_flight_fence, NULL);
-destroy_render_semaphore:
-    vkDestroySemaphore(device, render_finished_semaphore, NULL);
-destroy_image_semaphore:
-    vkDestroySemaphore(device, image_available_semaphore, NULL);
+destroy_flight_fences:
+    for(int i = 0; i < created_in_flight_fences; i++) {
+        vkDestroyFence(device, in_flight_fence_array[i], NULL);
+    }
+destroy_render_semaphores:
+    for(int i = 0; i < created_render_semaphores; i++) {
+        vkDestroySemaphore(device, render_finished_semaphore_array[i], NULL);
+    }
+destroy_image_semaphores:
+    for(int i = 0; i < created_image_semaphores; i++) {
+        vkDestroySemaphore(device, image_available_semaphore_array[i], NULL);
+    }
 destroy_pipeline:
     vkDestroyPipeline(device, pipeline, NULL);
 destroy_pipeline_layout:
